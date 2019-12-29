c5c2f374b6d76d7aae0e649a3a5ed5d1e8596fd1
Sunil <sunil10790@gmail.com>
2019-12-29 19:01:14 +0530

    Use notes.js and import it into app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const add = require('./utils.js');
+const getNotes = require('./notes.js');
 
-const sum = add(5,-3)
-console.log(sum);
+const msg = getNotes();
+console.log(msg);
--- utils.js
+++
@@ -1,8 +0,0 @@
-console.log("utils.js");
-
-const add = function(a, b) {
-    return a + b;
-}
-
-
-module.exports = add;
