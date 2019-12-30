ec6270afa8345ea341ae7fb494831ac504fcc23c
Sunil <sunil10790@gmail.com>
2019-12-30 20:07:07 +0530

    Install chalk package and use it

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const validator = require('validator');
+const chalk = require('chalk');
 
 const getNotes = require('./notes.js');
 
@@ -8,3 +9,6 @@ console.log(msg);
 console.log(validator.isEmail('sunil10790@gmail.com'));  // true
 console.log(validator.isEmail('sunil10790@gmail'));  // false
 console.log(validator.isURL('https://github.com/sunil10790/notes-app')); // true
+
+console.log(chalk.green('Success!!'));
+console.log(chalk.red.bold.inverse('Mogambo gone crAzy!!'));
