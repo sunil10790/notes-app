72dfeb1bc5e1550eee4bb9696adf4038d8173921
Sunil <sunil10790@gmail.com>
2019-12-30 19:40:46 +0530

    Intialize npm and gitignore. Import validator package.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const validator = require('validator');
+
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 console.log(msg);
+
+console.log(validator.isEmail('sunil10790@gmail.com'));  // true
+console.log(validator.isEmail('sunil10790@gmail'));  // false
+console.log(validator.isURL('https://github.com/sunil10790/notes-app')); // true
