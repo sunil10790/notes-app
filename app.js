83b28f18e599166265aad5f2531a7c12e107e128
Sunil <sunil10790@gmail.com>
2020-01-04 07:26:44 +0530

    Get user input from command line using process.argv

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const validator = require('validator');
 const chalk = require('chalk');
 
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+console.log(process.argv);
+console.log(process.argv[2]);
 
-console.log(validator.isEmail('sunil10790@gmail.com'));  // true
-console.log(validator.isEmail('sunil10790@gmail'));  // false
-console.log(validator.isURL('https://github.com/sunil10790/notes-app')); // true
-
-console.log(chalk.green('Success!!'));
-console.log(chalk.red.bold.inverse('Mogambo gone crAzy!!'));
+const operation = process.argv[2];
+if (operation === 'add') {
+    console.log("Adding a note!");
+} else if (operation === 'remove') {
+    console.log("Removing a note!");
+}
