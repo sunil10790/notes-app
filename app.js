7a819ab1810501a6c9867e02848585829ec86eb7
Sunil Kumar <sunil10790@outlook.com>
2021-09-02 20:49:03 +0530

    Install npm package: chalk. Use it.

--- app.js
+++ app.js
@@ -3,9 +3,16 @@
 // fs.appendFileSync('notes.txt', ' We can also append text to an existing file! :o');
 
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const note = getNotes();
-console.log(note);
+// const note = getNotes();
+// console.log(note);
 
-console.log(validator.isEmail('a@a.com'));
+
+// console.log(validator.isEmail('a@a.com'));
+
+
+console.log(chalk.green('Success!'));
+const log = console.log;
+log(chalk.green.bold.inverse('Success!'));
