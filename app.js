a7e937dc82cb97f2b6c167cfac589dbea01c495a
Sunil Kumar <sunil10790@outlook.com>
2021-09-02 08:51:33 +0530

    Importing our own file using module.exports and require

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
+// const fs = require('fs');
+// //fs.writeFileSync('notes.txt', 'This file is created and the text is added by node.js Awesomee!!');
+// fs.appendFileSync('notes.txt', ' We can also append text to an existing file! :o');
 
-//fs.writeFileSync('notes.txt', 'This file is created and the text is added by node.js Awesomee!!');
 
-fs.appendFileSync('notes.txt', ' We can also append text to an existing file! :o');
+const getNotes = require('./notes.js');
+const note = getNotes();
+console.log(note);
