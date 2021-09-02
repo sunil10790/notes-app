50ce1ae33a4531c75a9b955a787aaeac9cacb262
Sunil Kumar <sunil10790@outlook.com>
2021-09-02 14:45:20 +0530

    Initialize npm. Install npm package: validator

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 // //fs.writeFileSync('notes.txt', 'This file is created and the text is added by node.js Awesomee!!');
 // fs.appendFileSync('notes.txt', ' We can also append text to an existing file! :o');
 
-
+const validator = require('validator');
 const getNotes = require('./notes.js');
+
 const note = getNotes();
 console.log(note);
+
+console.log(validator.isEmail('a@a.com'));
