89be34198eb2142000445f5c25abcbda45a399f9
Sunil Kumar <sunil10790@outlook.com>
2021-09-09 09:27:24 +0530

    Add title and body for add note using yargs builder

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log("Adding a new note")
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log("Title: ", argv.title)
+        console.log("Body: ", argv.body)
     }
 })
 
